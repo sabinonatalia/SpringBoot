d0999c5c8e68e1f0b6c14555ff67ef4079947377
unknown <na.ssilva@outlook.com>
2021-06-08 00:17:49 -0300

    Atividade security 07.06

---
+++ BlogPessoal-1/src/main/java/br/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -0,0 +1,9 @@
+package br.org.generation.blogPessoal.seguranca;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class BasicSecurityConfig extends  WebSecurityConfigurerAdapter{
+
+}
